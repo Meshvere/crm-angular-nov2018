29975ea8f0eee0f19714f41cd2637f77734aa37f
Steph <steph.ruau@gmail.com>
2018-11-20 17:12:34 +0100

    Dispatch visualisation entre container et component

--- crm/src/app/prestations/prestations.module.ts
+++ crm/src/app/prestations/prestations.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from '../shared/shared.module';
 import { ListPrestationsComponent } from './containers/list-prestations/list-prestations.component';
 import { PagePrestationsComponent } from './pages/page-prestations/page-prestations.component';
 import { PrestationsRoutingModule } from './prestations-routing.module';
+import { PrestationComponent } from './components/prestation/prestation.component';
 
 @NgModule({
-  declarations: [PagePrestationsComponent, ListPrestationsComponent],
+  declarations: [PagePrestationsComponent, ListPrestationsComponent, PrestationComponent],
   imports: [
     CommonModule,
     PrestationsRoutingModule,
