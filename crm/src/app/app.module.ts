3a93b4a8b853c1f7c58ebfad412a425cea660773
sruau <sruau@sma-netagis.fr>
2019-01-07 14:09:54 +0100

    Essai webpack bundle analyzer

--- crm/src/app/app.module.ts
+++ crm/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { environment } from '../environments/environments.firebase';
+import { envFirebase } from '../environments/environments.firebase';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
@@ -26,7 +26,7 @@ registerLocaleData(localeFr);
     UiModule,
     NgbModule.forRoot(),
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp(envFirebase.firebase),
     AngularFirestoreModule,
     HttpClientModule
   ],
